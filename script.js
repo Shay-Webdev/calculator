886e7ac9d4af9b9574c1b9f1c3ced89882d5cbe1
shay-Webdev <181019353+Shay-Webdev@users.noreply.github.com>
2024-10-05 19:49:03 +0530

    Create html and css for layout and basic functions
    Created layout of calculator with display and other section.
    Created basic operation functions for addition, subtraction,
    multiplication, division and modulus.

---
+++ script.js
@@ -0,0 +1,28 @@
+function add (a, b) {
+    return a + b;
+}
+
+function subtract (a, b) {
+    return a - b;
+}
+
+function multiply (a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    return a/b;
+}
+
+function modulus (a, b){
+    return a%b;
+}
+
+let numberOne ;
+let numberTwo;
+let operand;
+
+function operate (num1, num2, operand1){
+
+}
+
