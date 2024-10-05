6d3d745d53f6c4093e9192f888c49adf6e8d28c6
shay-Webdev <181019353+Shay-Webdev@users.noreply.github.com>
2024-10-06 05:10:40 +0530

    Add function to display numbers and operands
    added onclick function to display numbers and operands in display.
    this way user could see what they press and input

--- script.js
+++ script.js
@@ -22,7 +22,25 @@ let numberOne ;
 let numberTwo;
 let operand;
 
+const inputButton = document.querySelectorAll("button");
+const displayPara = document.querySelector(".displayPara");
+
+
 function operate (num1, num2, operand1){
 
 }
+
+function input (input) {
+   let displayValue = displayPara.textContent = inputButton.value;
+   return displayValue;
+}
+inputButton.forEach((button) =>{
+    button.addEventListener("click", (input) => {
+        let buttonText = button.textContent;
+    
+        displayPara.textContent = buttonText;
+    
+    });
+});
+
 
